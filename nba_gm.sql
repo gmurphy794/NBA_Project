7119ae1fdae80e5dd7a4bb3a19f477863fbaabad
Shakeel Huda <huda.shakeel@gmail.com>
2018-11-20 12:58:12 -0500

    adding updates

---
+++ nba_gm.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE nba_db;
+
+USE nba_db;
+
+
+CREATE TABLE nba_info (
+	id INTEGER auto_increment primary KEY,
+    player_name TEXT,
+    salary INT,
+    season_start INT,
+    season_end INT,
+    team_name TEXT,
+    draft_rank INT,
+    draft INT,
+    position TEXT,
+    height TEXT,
+    weight FLOAT,
+    birth_date TEXT,
+    college TEXT
+);
+
